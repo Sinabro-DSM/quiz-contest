b3ac78533714642aa61e0d8ed4564471940cdbc6
TDM-RE <nye7181@gmail.com>
2018-10-07 15:27:25 +0900

    socket.io 서버 세팅

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv').config();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+require('dotenv').config();
 
 const port = process.env.PORT;
 
+io.on('connection', (socket) => {
+
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+});
 app.use(require('./routes'));
 
 app.listen(port, () => console.log(`server start at ${port}`));
