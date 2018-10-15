91e7183fc8165bfc82159aebead246b1d38ad9c7
milkyway002 <sea_bom_12@naver.com>
2018-10-15 11:31:49 +0900

    update ManagerWaiting.js real-time communication

--- web-front/Managerpage/js/ManagerWaiting.js
+++ web-front/Managerpage/js/ManagerWaiting.js
@@ -1,3 +1,11 @@
+window.onload = memberCounting;
+
+function memberCounting() {
+    let Member = 0;
+    document.getElementById('waiting-number').innerHTML = Member.toString() + '명';
+    setTimeout("memberCounting()", 1000);
+};
+
 function nextpage() {
     location.href = '';
 };
